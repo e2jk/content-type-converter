0de3e1129b5db53bc51038e3dc729565f8b6862d
Emilien Klein <emilien@klein.st>
2020-11-22 22:07:28 +0100

    Initial proxy behavior

--- app/xwwwformurlencoded2json/routes.py
+++ app/xwwwformurlencoded2json/routes.py
@@ -1,4 +1,5 @@
-from flask import current_app, render_template
+import requests
+from flask import Response, current_app, render_template, request
 
 from app.xwwwformurlencoded2json import bp
 
@@ -11,8 +12,9 @@ def index():
     return "Please indicate a profile:<br><ul>%s</ul>" % available_profiles
 
 
-@bp.route("/<profile>", methods=["GET", "POST"])
-def profile(profile):
+@bp.route("/<profile>", defaults={"path": ""}, methods=["GET", "POST"])
+@bp.route("/<profile>/<path:path>", methods=["GET", "POST"])
+def profile(profile, path):
     if profile not in current_app.config["PROFILES"]:
         # Invalid profile
         return (
@@ -22,3 +24,27 @@ def profile(profile):
             ),
             404,
         )
+    base_url = current_app.config["PROFILES"][profile]["base_url"]
+    url = f"{base_url}{path}"
+    if request.query_string:
+        url += "?%s" % request.query_string.decode("utf-8")
+
+    # Perform the request
+    if request.method == "GET":
+        r = requests.get(url)
+    elif request.method == "POST":
+        # TODO: handle POST requests' payload
+        r = requests.post(url)
+    else:
+        # TODO: support other HTTP methods
+        return (f"Unsupported method {request.method}", 500)
+
+    # Edit the content to make relative links go through our proxy
+    base_path = f"/xwwwformurlencoded2json/{profile}/"
+    content = r.content.decode("utf-8")  # Bytes to UTF-8 string
+    content = content.replace('href="', f'href="{base_path}')
+    # Make sure full URL links are not funneled through our proxy
+    content = content.replace(f'href="{base_path}http', 'href="http')
+    content = content.encode("utf-8")  # Back to bytes
+
+    return Response(content, r.status_code, mimetype=r.headers["Content-Type"])
--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     PROFILES = {
-        "test_profile": {"aa": 1, "bb": 2},
-        "second_profile": {"aa": 1, "bb": 2},
+        "test_profile": {"base_url": "http://myhttpheader.com/", "aa": 1, "bb": 2},
+        "second_profile": {"base_url": "http://example.com/", "aa": 1, "bb": 2},
     }
