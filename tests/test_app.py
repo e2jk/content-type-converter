2bca1bcc7593077a9c9769b97cffa8445d96bf98
Emilien Klein <emilien@klein.st>
2020-12-12 18:31:11 +0100

    Missing tests for the initial proxy behavior 0de3e1129b5db53bc51038e3dc729565f8b6862d

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 
+import app
 from app import create_app
 from config import Config
 
@@ -35,3 +36,24 @@ def test_xwwwformurlencoded2json_dummy(client):
     rv = client.get("/xwwwformurlencoded2json/dummy")
     assert b"<h1>Invalid profile &#39;dummy&#39;</h1>" in rv.data
     assert b"This profile does not exist" in rv.data
+
+
+def test_xwwwformurlencoded2json_get(client, mocker):
+    mocker.patch("app.xwwwformurlencoded2json.routes.requests.get")
+    client.get("/xwwwformurlencoded2json/test_profile?hello=world&test=1")
+    app.xwwwformurlencoded2json.routes.requests.get.assert_called_once_with(
+        "http://myhttpheader.com/?hello=world&test=1"
+    )
+
+
+def test_xwwwformurlencoded2json_post(client, mocker):
+    mocker.patch("app.xwwwformurlencoded2json.routes.requests.post")
+    client.post("/xwwwformurlencoded2json/test_profile?hello=world&test=1")
+    app.xwwwformurlencoded2json.routes.requests.post.assert_called_once_with(
+        "http://myhttpheader.com/?hello=world&test=1"
+    )
+
+
+def test_xwwwformurlencoded2json_invalid_method(client, mocker):
+    rv = client.head("/xwwwformurlencoded2json/test_profile?hello=world&test=1")
+    assert rv.status_code == 500
