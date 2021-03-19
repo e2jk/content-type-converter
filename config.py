3719e8ae357c205fd0653c39f65de794ecffeb83
Emilien Klein <emilien@klein.st>
2021-03-19 21:11:01 +0100

    Second test profile

--- config.py
+++ config.py
@@ -5,5 +5,9 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     PROFILES = {
         "test_profile": {"base_url": "http://myhttpheader.com/", "aa": 1, "bb": 2},
-        "second_profile": {"base_url": "http://example.com/", "aa": 1, "bb": 2},
+        "second_profile": {
+            "base_url": "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending",
+            "aa": 1,
+            "bb": 2,
+        },
     }
